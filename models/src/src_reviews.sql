f6693c5aab723901ac2d593a6e4714784e8fd7af
kirtanshah33 <59151394+kirtanshah33@users.noreply.github.com>
2023-06-16 11:24:39 +0530

    dbt model for airbnb dataset

---
+++ models/src/src_reviews.sql
@@ -0,0 +1,14 @@
+WITH raw_reviews AS (
+ SELECT
+ *
+ FROM
+ {{ source('airbnb', 'reviews') }}
+)
+SELECT
+LISTING_ID,
+DATE AS REVIEW_DATE,
+REVIEWER_NAME,
+COMMENTS AS REVIEW_TEXT,
+SENTIMENT AS REVIEW_SENTIMENT
+FROM
+raw_reviews
